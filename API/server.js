1f8524259e97779824d977022e873804659e4db8
SHNIPPED <narizh4@gmail.com>
2025-07-18 09:31:58 +0500

    new_button

--- API/server.js
+++ API/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import mysql  from"mysql2";
-import cors  from'cors';
-import bodyParser  from'body-parser';
-import AuthRouter  from'./routes/AuthRouter.js'; 
+import mysql from "mysql2";
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import AuthRouter from './routes/AuthRouter.js';
 
 const app = express();
 
@@ -11,43 +11,43 @@ app.use(bodyParser.json());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-const corsOptions ={
-  origin:'*', 
-  credentials:true,            
-  optionSuccessStatus:200,
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
 }
 
 app.use(cors(corsOptions))
 
 app.listen(3001, error => {
-	error ? console.log(error) : console.log(`listening port ${3001}`);
+  error ? console.log(error) : console.log(`listening port ${3001}`);
 });
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "des",
-    password: "Asdfg123"
+  host: "localhost",
+  user: "root",
+  database: "des",
+  password: "Asdfg123"
 });
 
-connection.connect(function(err){
-    if (err) {
-      return console.error("Ошибка: " + err.message);
-    }
-    else{
-      console.log("Подключение к серверу MySQL успешно установлено");
-    }
+connection.connect(function (err) {
+  if (err) {
+    return console.error("Ошибка: " + err.message);
+  }
+  else {
+    console.log("Подключение к серверу MySQL успешно установлено");
+  }
 });
 
 
-app.get('/',  (req, res) => {
-    connection.query("SELECT * FROM des.hosts",(error,result) =>{
+app.get('/', (req, res) => {
+  connection.query("SELECT * FROM des.hosts", (error, result) => {
     res.json(result)
   })
 });
 
-app.delete('/delete/:id',  (req, res) => {
-  const {id} = req.params;
+app.delete('/delete/:id', (req, res) => {
+  const { id } = req.params;
   if (isNaN(id)) {
     return res.status(400).json({ message: 'Некорректный ID' });
   }
@@ -55,64 +55,62 @@ app.delete('/delete/:id',  (req, res) => {
   const query = 'DELETE FROM des.hosts WHERE id = ?';
 
   connection.query(query, [id], (error, result) => {
-      if (error) {
-          console.error('Ошибка при удалении записи:', error);
-          return res.status(500).json({ message: 'Ошибка сервера' });
-      }
+    if (error) {
+      console.error('Ошибка при удалении записи:', error);
+      return res.status(500).json({ message: 'Ошибка сервера' });
+    }
 
-      if (result.affectedRows > 0) {
-          return res.status(200).json({ message: `Удалено записей: ${result.affectedRows}` });
-      } else {
-          return res.status(404).json({ message: 'Запись не найдена' });
-      }
+    if (result.affectedRows > 0) {
+      return res.status(200).json({ message: `Удалено записей: ${result.affectedRows}` });
+    } else {
+      return res.status(404).json({ message: 'Запись не найдена' });
+    }
   })
 });
 
-app.post('/create',     (req,res) =>{
-   const {fcs,host} = req.body;
-   if(!fcs || !host){
-    return res.status(400).json({message: 'Необходимо указать ФИО и хост'});
-   }
+app.post('/create', (req, res) => {
+  const { fcs, host } = req.body;
+  if (!fcs || !host) {
+    return res.status(400).json({ message: 'Необходимо указать ФИО и хост' });
+  }
 
-    const query = "INSERT INTO `des`.`hosts` (`fcs`, `host`) VALUES (?, ?);";
+  const query = "INSERT INTO `des`.`hosts` (`fcs`, `host`) VALUES (?, ?);";
 
-    connection.query(query,[fcs,host],(error,result) =>{
-      if(error){
-          console.error('Ошибка при добавлении записи:', error);
-          return res.status(500).json({message: 'Ошибка сервера'});
-      }
-
-      if(result.affectedRows >0){
-        return res.status(200).json({message: `Добавлена запись: ${result.affectedRows}`});
-      }else{
-        return res.status(404).json({message:'Запись не добавлена'});
-      }
-    })
-   });
-
-   app.post('/edit/:id',   (req,res) =>{
-    const {fcs,host} = req.body;
-    const {id} = req.params;
-    if(!fcs || !host || !id){
-     return res.status(400).json({message: 'Необходимо указать ФИО и хост'});
+  connection.query(query, [fcs, host], (error, result) => {
+    if (error) {
+      console.error('Ошибка при добавлении записи:', error);
+      return res.status(500).json({ message: 'Ошибка сервера' });
     }
- 
-     const query = 'UPDATE des.hosts SET `fcs` = ?, `host` = ? WHERE (`id` = ?);';
- 
-     connection.query(query,[fcs,host,id],(error,result) =>{
-       if(error){
-           console.error('Ошибка при изменении записи:', error);
-           return res.status(500).json({message: 'Ошибка сервера'});
-       }
- 
-       if(result.affectedRows >0){
-         return res.status(200).json({message: `Изменена запись: ${result.affectedRows}`});
-       }else{
-         return res.status(404).json({message:'Запись не изменена'});
-       }
-     })
-    });
- 
 
+    if (result.affectedRows > 0) {
+      return res.status(200).json({ message: `Добавлена запись: ${result.affectedRows}` });
+    } else {
+      return res.status(404).json({ message: 'Запись не добавлена' });
+    }
+  })
+});
+
+app.post('/edit/:id', (req, res) => {
+  const { fcs, host } = req.body;
+  const { id } = req.params;
+  if (!fcs || !host || !id) {
+    return res.status(400).json({ message: 'Необходимо указать ФИО и хост' });
+  }
+
+  const query = 'UPDATE des.hosts SET `fcs` = ?, `host` = ? WHERE (`id` = ?);';
+
+  connection.query(query, [fcs, host, id], (error, result) => {
+    if (error) {
+      console.error('Ошибка при изменении записи:', error);
+      return res.status(500).json({ message: 'Ошибка сервера' });
+    }
+
+    if (result.affectedRows > 0) {
+      return res.status(200).json({ message: `Изменена запись: ${result.affectedRows}` });
+    } else {
+      return res.status(404).json({ message: 'Запись не изменена' });
+    }
+  })
+});
 
 app.use(AuthRouter);
--- UI/react-intro/src/components/table/table.js
+++ UI/react-intro/src/components/table/table.js
@@ -1,179 +1,184 @@
-import React, { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom';
-import './table.css';
-import trash from '../pictures/trash.png'
-import edit from '../pictures/edit.png'
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./table.css";
+import trash from "../pictures/trash.png";
+import edit from "../pictures/edit.png";
 
-function Table(){
-    const [fcs, setFCS] = useState('');
-    const [host, setHost] = useState('');
-    const [editingUser, setEditingUser] = useState(null);
-    const [hosts, setHosts] = useState([]);
-    const navigate = useNavigate();
+function Table() {
+  const [fcs, setFCS] = useState("");
+  const [host, setHost] = useState("");
+  const [editingUser, setEditingUser] = useState(null);
+  const [hosts, setHosts] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (!token) {
-            navigate('/login');
-        }
-    }, [navigate]);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/login");
+    }
+  }, [navigate]);
 
+  const fetchData = () => {
+    fetch("http://192.168.19.50:3001/")
+      .then((response) => response.json())
+      .then((data) => setHosts(data))
+      .catch((error) => console.error("Ошибка при загрузке данных:", error));
+  };
 
-    const fetchData = () => {
-        fetch("http://192.168.19.50:3001/")
-            .then(response => response.json())
-            .then(data => setHosts(data))
-            .catch(error => console.error('Ошибка при загрузке данных:', error));
-    };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!fcs || !host) {
+      alert("Пожалуйста, заполните все поля");
+      return;
+    }
+
+    const userData = { fcs, host };
+
+    try {
+      let response;
+      if (editingUser) {
+        console.log(userData);
+        response = await fetch(
+          `http://192.168.19.50:3001/edit/${editingUser.id}`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData),
+          }
+        );
+      } else {
+        response = await fetch("http://192.168.19.50:3001/create", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(userData),
+        });
+      }
+
+      const data = await response.json();
+      alert(data.message);
+      setFCS("");
+      setHost("");
+      setEditingUser(null);
+      fetchData();
+    } catch (error) {
+      console.error("Ошибка при отправке данных:", error);
+      alert("Произошла ошибка");
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      const response = await fetch(`http://192.168.19.50:3001/delete/${id}`, {
+        method: "DELETE",
+      });
+      if (response.ok) {
         fetchData();
-    }, []);
+      } else {
+        const errorData = await response.json();
+        alert(`Ошибка: ${errorData.message}`);
+      }
+    } catch (error) {
+      console.error("Ошибка:", error);
+    }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const startEditing = (user) => {
+    setEditingUser(user);
+    setFCS(user.fcs);
+    setHost(user.host);
+  };
 
-        if (!fcs || !host) {
-            alert('Пожалуйста, заполните все поля');
-            return;
-        }
+  const DisplayData = hosts.map((info) => (
+    <tbody key={info.id} className="table_host">
+      <tr className="th_host">
+        <td className="tb_host">{info.id}</td>
+        <td className="tb_host">{info.fcs}</td>
+        <td className="tb_host host-cell">
+          {info.host}
+          <div className="host-div">
+            <button className="host-button" onClick={() => startEditing(info)}>
+              <img className="host-button" src={edit} alt="Edit" />
+            </button>
+            <button
+              className="host-button"
+              onClick={() => handleDelete(info.id)}
+            >
+              <img className="host-button" src={trash} alt="Delete" />
+            </button>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  ));
 
-        const userData = { fcs, host };
-
-        try {
-            let response;
-            if (editingUser) {
-                console.log(userData)
-                response = await fetch(`http://192.168.19.50:3001/edit/${editingUser.id}`, {
-                    method: 'POST',
-                    headers: {
-                      'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(userData),
-                });
-            } else {
-
-                response = await fetch('http://192.168.19.50:3001/create', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(userData),
-                });
-            }
-
-                const data = await response.json();
-                alert(data.message);
-                setFCS('');
-                setHost('');
-                setEditingUser(null);
-                fetchData();
-        } catch (error) {
-            console.error('Ошибка при отправке данных:', error);
-            alert('Произошла ошибка');
-        }
-    };
-
-    const handleDelete = async (id) => {
-        try {
-            const response = await fetch(`http://192.168.19.50:3001/delete/${id}`, {
-                method: 'DELETE',
-            });
-            if (response.ok) {
-                fetchData();
-            } else {
-                const errorData = await response.json();
-                alert(`Ошибка: ${errorData.message}`);
-            }
-        } catch (error) {
-            console.error('Ошибка:', error);
-        }
-    };
-
-    const startEditing = (user) => {
-        setEditingUser(user);
-        setFCS(user.fcs)
-        setHost(user.host)
-    };
-
-
-    const DisplayData = hosts.map((info) => (
-        <tbody key={info.id} className='table_host'>
-            <tr className='th_host'>
-                <td className='tb_host'>{info.id}</td>
-                <td className='tb_host'>{info.fcs}</td>
-                <td className='tb_host host-cell'>
-                    {info.host}
-                    <div className="host-div">
-                        <button className="host-button" onClick={() => startEditing(info)}>
-                            <img className="host-button" src={edit} alt="Edit" />
-                        </button>
-                        <button className="host-button" onClick={() => handleDelete(info.id)}>
-                            <img className="host-button" src={trash} alt="Delete" />
-                        </button>
-                    </div>
-                </td>
+  return (
+    <div className="div_table">
+      <div className="table">
+        <table>
+          <thead className="thead">
+            <tr>
+              <th>№</th>
+              <th>ФИО</th>
+              <th>Хост</th>
             </tr>
-        </tbody>
-    ));
-
-    return (
-        <div className="div_table">
-            <div className="table">
-                <table>
-                    <thead className="thead">
-                        <tr>
-                            <th>№</th>
-                            <th>ФИО</th>
-                            <th>Хост</th>
-                        </tr>
-                    </thead>
-                    {DisplayData}
-                </table>
-            </div>
-            <div>
-              <div class="div-form">
-                  <form className="login-form" onSubmit={handleSubmit}>
-                      <div className="form-group">
-                          <label htmlFor="fcs">ФИО</label>
-                          <input
-                              type="text"
-                              id="fcs"
-                              value={fcs}
-                              onChange={(e) => setFCS(e.target.value)}
-                              placeholder="Введите ФИО"
-                              required
-                          />
-                      </div>
-                      <div className="form-group">
-                          <label htmlFor="host">Хост</label>
-                          <input
-                              type="text"
-                              id="host"
-                              value={host}
-                              onChange={(e) => setHost(e.target.value)}
-                              placeholder="Введите хост"
-                              required
-                          />
-                      </div>
-                      <button type="submit" className="login-button">
-                          {editingUser ? 'Изменить' : 'Добавить пользователя'}
-                      </button>
-                  </form>
+          </thead>
+          {DisplayData}
+        </table>
+      </div>
+      <div>
+        <div>
+          <div class="div-form">
+            <form className="login-form" onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="fcs">ФИО</label>
+                <input
+                  type="text"
+                  id="fcs"
+                  value={fcs}
+                  onChange={(e) => setFCS(e.target.value)}
+                  placeholder="Введите ФИО"
+                  required
+                />
               </div>
-            </div>
+              <div className="form-group">
+                <label htmlFor="host">Хост</label>
+                <input
+                  type="text"
+                  id="host"
+                  value={host}
+                  onChange={(e) => setHost(e.target.value)}
+                  placeholder="Введите хост"
+                  required
+                />
+              </div>
+              <button type="submit" className="login-button">
+                {editingUser ? "Изменить" : "Добавить пользователя"}
+              </button>
+            </form>
+          </div>
         </div>
-    );
-};
+      </div>
+    </div>
+  );
+}
 
-export default Table
+export default Table;
 
-   // {
-       // "id": 40,
-     //   "fcs": "Романова Светлана Сергеевна",
-      //  "post": "Ведущий документовед",
-     //   "phone_number": "207-63-56 (707)",
-     //   "Email": "ssromanova@des.permkrai.ru",
-    //    "addres": "ул.Крупской,5 каб.104",
-   //     "deport": "0"
-   // },
+// {
+// "id": 40,
+//   "fcs": "Романова Светлана Сергеевна",
+//  "post": "Ведущий документовед",
+//   "phone_number": "207-63-56 (707)",
+//   "Email": "ssromanova@des.permkrai.ru",
+//    "addres": "ул.Крупской,5 каб.104",
+//     "deport": "0"
+// },
